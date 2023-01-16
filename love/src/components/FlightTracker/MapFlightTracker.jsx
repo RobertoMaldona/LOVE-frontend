415cf77a8276a2273a5e7b9dd8cf57b2d68f6e2c
MJ MT <maria.mellado@ug.uchile.cl>
2023-01-16 18:02:02 -0300

    Updated map for each radius required with telescope icon updated too

--- love/src/components/FlightTracker/MapFlightTracker.jsx
+++ love/src/components/FlightTracker/MapFlightTracker.jsx
@@ -41,7 +41,7 @@ export default class MapFlightTracker extends Component {
     const projection = d3
       .geoMercator()
       .center(telescopeCoords)
-      .scale(width * 25.2) // scale; 13 - 200 km,  16.05 -160 km and 25.2 -100 km.
+      .scale(width * 13) // scale; 13 - 200 km,  16.05 -160 km and 25.2 -100 km.
       .translate([width / 2, height / 2 + 15]);
 
     let geoGenerator = d3.geoPath().projection(projection);
@@ -59,15 +59,15 @@ export default class MapFlightTracker extends Component {
             .select('#TelescopeDiv #Paths')
             .attr('width', width)
             .attr('height', height)
-            .style('background-color', '#2e3e47')
-            .style('opacity', '90%');
+            .classed(styles.circle, true);
 
           Coquimbo.features.map((feature) => {
             svg
               .append('path')
               .attr('id', feature.properties['NOM_COM'])
               .attr('d', geoGenerator(feature))
-              .attr('border', '#4c606a');
+              .style('fill', '#2e3e47')
+              .style('stroke', '#4c606a');
           });
 
           Atacama.features.map((feature) => {
@@ -75,7 +75,8 @@ export default class MapFlightTracker extends Component {
               .append('path')
               .attr('id', feature.properties['NOM_COM'])
               .attr('d', geoGenerator(feature))
-              .attr('border', '#4c606a');
+              .style('fill', '#2e3e47')
+              .style('stroke', '#4c606a');
           });
 
           Valparaiso.features.map((feature) => {
@@ -83,7 +84,8 @@ export default class MapFlightTracker extends Component {
               .append('path')
               .attr('id', feature.properties['NOM_COM'])
               .attr('d', geoGenerator(feature))
-              .attr('border', '#4c606a');
+              .style('fill', '#2e3e47')
+              .style('stroke', '#4c606a');
           });
 
           // first zone : 200 km area.
@@ -99,24 +101,14 @@ export default class MapFlightTracker extends Component {
             .attr('cx', '250')
             .attr('cy', '250')
             .attr('r', ' 220') // this radius let encloses the last radius in map. For scales (222, 220).
-            .attr('stroke', 'black')
-            .attr('strokeWidth', '2')
             .attr('fill', 'black');
 
           svg
             .append('rect')
             .attr('mask', 'url(#Mask)')
             .classed(styles.rect, true)
-            .attr('width', '500')
-            .attr('height', '500');
-
-          svg
-            .append('circle')
-            .attr('id', 'external_circle')
-            .attr('cx', '250')
-            .attr('cy', '250')
-            .attr('r', '221.42696271805653')
-            .attr('fill', 'none');
+            .attr('width', `${width}`)
+            .attr('height', `${height}`);
 
           // second zone : 160 km area.
           // const long_lat_2 = [-69.79391230365658, -29.057079010258132]
@@ -127,32 +119,42 @@ export default class MapFlightTracker extends Component {
           svg
             .append('circle')
             .attr('id', 'middle_circle')
-            .attr('cx', '250')
-            .attr('cy', '250')
-            .attr('r', '219.84341506903084')
+            .attr('cx', `${width / 2}`)
+            .attr('cy', `${height / 2}`)
+            .attr('r', '175.79622153494552')
             .attr('stroke', '#bcd8e2')
             .attr('stroke-width', '1')
             .attr('fill', 'none');
 
           // third zone : 100 km area.
-          const long_lat_3 = [-69.93171344197097, -29.672737573022122];
-          const coords_3 = projection(long_lat_3);
-          console.log(coords_3); //returns [341.3675737065496, 190.66005738475087], [362.80381215308626, 173.21876315578902], [427.11252749269624, 120.89488046890165]
+          // const long_lat_3 = [-69.93171344197097, -29.672737573022122];
+          // const coords_3 = projection(long_lat_3);
+          // console.log(coords_3); //returns [341.3675737065496, 190.66005738475087], [362.80381215308626, 173.21876315578902], [427.11252749269624, 120.89488046890165]
           // // const radius_3 = 108.94614410158273, 136.45533469819196, 219.17340003748237  with Euclidian distance.
 
           svg
             .append('circle')
             .attr('id', 'intern_circle')
-            .attr('cx', '250')
-            .attr('cy', '250')
-            .attr('r', '219.17340003748237')
+            .attr('cx', `${width / 2}`)
+            .attr('cy', `${height / 2}`)
+            .attr('r', '108.94614410158273')
             .attr('stroke', '#bcd8e2')
             .attr('stroke-width', '1')
             .attr('fill', 'none');
 
+          // this circle depends on external circle.
+          svg
+            .append('circle')
+            .attr('id', 'external_circle')
+            .attr('cx', `${width / 2}`)
+            .attr('cy', `${height / 2}`)
+            .attr('r', '221.42696271805653')
+            .attr('fill', '#bcd8e2')
+            .style('opacity', '10%');
+
           // telescope icon.
           const pathTelescope =
-            'm13.02,4.99l-4.01-.02v-.63l-.88-2.96-.21-.16-.05-.17-.89-.6h-.12l-.46-.32h-1.63v.16h-2.42l-.22-.16H.41v.13h.17v.16l-.46.03v5.36l.55.04v5.03h5.15c.45.92,2.22,3.75,2.81,4.69h0l.14.25.14.25.14-.25.14-.25h0c.59-.94,2.36-3.77,2.81-4.69h6.31v-2.7l-5.29-3.19Z';
+            'm.12,19.45v1.14h.31v1.66s.62.01,1.58.03v20.68h17.78c1.55,3.17,7.66,12.94,9.69,16.18h-.02l.5.87.5.87.5-.87.5-.87h-.02c2.03-3.23,8.14-13.01,9.69-16.18h21.78v-9.33l-18.26-11.01-17.45-.07v-.31c.11,0,.17,0,.17,0v-4.5h1.23l2.2-.32v-2.74l-3.03-10.21-.74-.55-.19-.6-3.06-2.08h-.42l-1.6-1.11h-5.62l-.02.55H7.8l-.78-.55H1.12l.02.46h.55v.55l-1.57.09';
 
           svg
             .append('g')
@@ -160,12 +162,21 @@ export default class MapFlightTracker extends Component {
             .append('path')
             .attr('id', 'telescopeIconP')
             .attr('d', pathTelescope)
-            .attr('transform', `translate(${250 - 9},${250 - 8}) scale(${1})`)
+            .attr('transform', `translate(${250 - 31.4 / 2},${250 - 30.38 / 2})  scale(0.5)`)
             .style('fill', '#bcd8e2')
-            .style('stroke', '#bcd8e2')
-            .style('stroke-miterlimit', '10')
-            .style('stroke-width', '10px')
-            .style('opacity', '100%');
+            .style('stroke', '#bcd8e2');
+
+          // La Serena.
+          const long_lat_serena = [-71.25715298618236, -29.89192170340795];
+          const coords_serena = projection(long_lat_serena);
+
+          svg
+            .append('circle')
+            .attr('id', 'circle_serena')
+            .attr('cx', `${coords_serena[0]}`)
+            .attr('cy', `${coords_serena[1]}`)
+            .attr('r', '5')
+            .classed(styles.laSerena, true);
         });
       });
     });
@@ -180,17 +191,17 @@ export default class MapFlightTracker extends Component {
     return (
       <>
         {/* this is for generate the static map */}
-        <div className={styles.container}>
+        {/* <div className={styles.container}>
           <div>{this.getRegionSvg()}</div>
           <div id="TelescopeDiv">
             <svg id="Paths" className={styles.CoquimboSvg}></svg>
           </div>
-        </div>
+        </div> */}
 
         {/* run this for load the static map */}
-        {/* <div id="TelescopeDiv">
-            <Map></Map>
-          </div> */}
+        <div id="TelescopeDiv">
+          <Map></Map>
+        </div>
       </>
     );
   }
